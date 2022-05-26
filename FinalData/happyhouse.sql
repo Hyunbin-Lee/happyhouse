1b343f069bf3207877b085c82221a9629f0d20ef
DESKTOP-9FLSHCQ\노영석 <seri5051@naver.com>
2022-05-26 10:03:09 +0900

    final data 공유 폴더 생성 및 sql commit

---
+++ FinalData/happyhouse.sql
@@ -0,0 +1,136 @@
+-- baseaddress create statement --
+CREATE TABLE `baseaddress` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `sidoName` varchar(30) DEFAULT NULL,
+  `gugunName` varchar(30) DEFAULT NULL,
+  `dongName` varchar(30) DEFAULT NULL,
+  `dongCode` varchar(10) DEFAULT NULL,
+  `lat` varchar(20) DEFAULT NULL,
+  `lng` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  KEY `dongCode` (`dongCode`),
+  CONSTRAINT `baseaddress_ibfk_1` FOREIGN KEY (`dongCode`) REFERENCES `dongcode` (`dongCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=3597 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- board create statement --
+CREATE TABLE `board` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `rg_date` datetime DEFAULT NULL,
+  `title` varchar(50) NOT NULL,
+  `contents` text,
+  `author` varchar(30) NOT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- dongcode create statement --
+CREATE TABLE `dongcode` (
+  `dongCode` varchar(10) NOT NULL,
+  `sidoName` varchar(30) DEFAULT NULL,
+  `gugunName` varchar(30) DEFAULT NULL,
+  `dongName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`dongCode`),
+  KEY `sidoName` (`sidoName`),
+  CONSTRAINT `dongcode_ibfk_1` FOREIGN KEY (`sidoName`) REFERENCES `sidocode` (`sidoName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- favregion create statement --
+CREATE TABLE `favregion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_favRegion_user_idx` (`user_id`),
+  CONSTRAINT `fk_favRegion_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- guguncode create statement --
+CREATE TABLE `guguncode` (
+  `gugunCode` varchar(10) NOT NULL,
+  `gugunName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`gugunCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- housedeal create statement --
+CREATE TABLE `housedeal` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `aptCode` int DEFAULT NULL,
+  `dealAmount` varchar(20) DEFAULT NULL,
+  `dealYear` int DEFAULT NULL,
+  `dealMonth` int DEFAULT NULL,
+  `dealDay` int DEFAULT NULL,
+  `area` varchar(30) DEFAULT NULL,
+  `floor` varchar(30) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `rentMoney` varchar(30) DEFAULT NULL,
+  `isfav` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`no`),
+  KEY `aptCode` (`aptCode`),
+  CONSTRAINT `housedeal_ibfk_1` FOREIGN KEY (`aptCode`) REFERENCES `houseinfo` (`aptCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=68865 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- houseinfo create statement --
+CREATE TABLE `houseinfo` (
+  `aptCode` int NOT NULL,
+  `aptName` varchar(50) DEFAULT NULL,
+  `dongCode` varchar(10) DEFAULT NULL,
+  `dongName` varchar(30) DEFAULT NULL,
+  `buildYear` int DEFAULT NULL,
+  `jibun` varchar(30) DEFAULT NULL,
+  `lat` varchar(30) DEFAULT NULL,
+  `lng` varchar(30) DEFAULT NULL,
+  `img` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`aptCode`),
+  KEY `dongCode` (`dongCode`),
+  CONSTRAINT `houseinfo_ibfk_1` FOREIGN KEY (`dongCode`) REFERENCES `dongcode` (`dongCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- member create statement --
+CREATE TABLE `member` (
+  `id` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- qna create statement --
+CREATE TABLE `qna` (
+  `articleno` int NOT NULL AUTO_INCREMENT,
+  `userid` varchar(16) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `content` varchar(2000) DEFAULT NULL,
+  `answer` varchar(2000) DEFAULT NULL,
+  `hit` int DEFAULT '0',
+  `regtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- sidocode create statement --
+CREATE TABLE `sidocode` (
+  `sidoCode` varchar(10) NOT NULL,
+  `sidoName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`sidoCode`),
+  UNIQUE KEY `sidoName` (`sidoName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- user create statement --
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
